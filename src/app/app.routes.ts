76f78a3268c69b7b768594313905933e9ab1b13d
Axel W Notebook <axelwestman@outlook.com>
2025-01-17 10:23:55 -0300

    Agregado icono carrito y componente carrito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ContentComponent } from './content/content.component';
 import { HomeComponent } from './home/home.component';
+import { CarritoComponent } from './carrito/carrito.component';
 
 export const routes: Routes = [
     {path: 'content', component: ContentComponent},
+    {path: 'carrito', component: CarritoComponent},
     {path: 'home', component: HomeComponent },
     {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
