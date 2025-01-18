cb008e4d5cb483fe4a7afc0b98a2d673337c56a4
Axel W Notebook <axelwestman@outlook.com>
2025-01-18 17:03:03 -0300

    Opciones de productos en la sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,51 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatDrawerMode,MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {MatTreeModule} from '@angular/material/tree';
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Productos',
+    children: [
+      {
+        name: 'Ropa',
+        children: [{name: 'Remeras'}, {name: 'Blusas'}, {name: 'Camisas'}, {name: 'Camisolas'}, {name: 'Túnicas'}, {name: 'Vestidos'} , {name: 'Soleros'}, {name: 'Palazos'}, {name: 'Babuchas'} , {name: 'Leggings'}, {name: 'Calza'}, {name: 'Shorts'}, {name: 'Bermudas'}, {name: 'Polleras'}, {name: 'Faldas'}],
+      },
+      {
+        name: 'Accesorios',
+        children: [{name: 'Pareo'}, {name: 'Pañuelos'}, {name: 'Pashminas'}, {name: 'Sombreros'}, {name: 'Capelinas'}, {name: 'Ruanas'}, {name: 'Mantones'}, {name: 'Bufandas'}, {name: 'Cuellos'}, {name: 'Japamalas Indias'}, {name: 'Collares'}, {name: 'Sobres'}, {name: 'Monederos Pakistan'}, {name: 'Carteras'}, {name: 'Bolsos'}],
+      },{
+        name: 'Sahumerios',
+        children: [{name: 'Satya'}, {name: 'Goloka'}, {name: 'Namaste'}, {name: 'Thales'}, {name: 'Mistic Spirits'}, {name: 'Sagrada Madre'}]
+      },{
+        name: 'Decoración',
+        children: [{name: 'Manta'}, {name: 'Corazón individual'}, {name: 'Corazón doble'}, {name: 'Corazón triple'}, {name: 'Tira colgante India'} ]
+      }
+    ],
+  },
+];
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, RouterLink, RouterLinkActive ,MatToolbarModule,MatListModule, MatButtonModule, MatSidenavModule, MatIconModule],
+  imports: [ RouterOutlet, RouterLink, RouterLinkActive ,MatToolbarModule,MatListModule, MatButtonModule, MatSidenavModule, MatIconModule, MatTreeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'colombina-web';
+
+  dataSource = TREE_DATA;
+
+  childrenAccessor = (node: FoodNode) => node.children ?? [];
+
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+
+
 }
