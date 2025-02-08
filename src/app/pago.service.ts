597a8734d6d00ac017bb8c25f898fb981c6bc60e
Axel Westman <111924075+AxelWestman@users.noreply.github.com>
2025-02-08 19:40:12 -0300

    Modificando la parte del pago donde se ponen los datos del comprador

--- src/app/pago.service.ts
+++ src/app/pago.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PagoService {
 
-  constructor() { }
+
+   getCordobaCiudades= async () => {
+    const response = await fetch('/cordobaCiudades.json', {
+      method: "get",
+    });
+    try {
+      const data = await response.json();
+      // Transforma el array de objetos en un array de nombres
+      const nombres = data.map((ciudad: { nombre: any; }) => ciudad.nombre);
+      return nombres;
+    } catch (err) {
+      console.log(err);
+      throw err; // Relanza el error para manejarlo en el componente
+    }
+  }
+
+  constructor(private http: HttpClient) {}
 }
