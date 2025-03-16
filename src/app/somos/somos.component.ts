baf6e2f5f984679e3f15d8332bc7c16fd36d7bef
Axel Westman <111924075+AxelWestman@users.noreply.github.com>
2025-03-16 11:20:49 -0300

    añadido de texto para "quienes somos"

--- src/app/somos/somos.component.ts
+++ src/app/somos/somos.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-somos',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent,  RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './somos.component.html',
   styleUrl: './somos.component.scss'
 })
